a8268f2e6181930e13311098624fd35af718e998
Jônatas Silvério <daniel_jhonny@hotmail.com>
2025-05-10 21:05:41 +0000

    feature visualização 360

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/view360')
+def view360():
+    return render_template('view360.html')
+
