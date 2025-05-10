f91fdd1ac8e7fa18361c5333f5bdc347923f8de8
Jônatas Silvério <daniel_jhonny@hotmail.com>
2025-05-10 19:56:43 +0000

    add homepage

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello, World! Insta 360 Soon!!'
+def index():
+    return render_template('index.html')
