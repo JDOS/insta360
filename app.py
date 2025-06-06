0ca9865dc2ccf226fc176b96ddc412a752b46e49
Jônatas Silvério <daniel_jhonny@hotmail.com>
2025-06-06 20:50:25 +0000

    add feature animacao, zoom maximo, parameter url

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#from pathlib import Path
 
 app = Flask(__name__)
 
@@ -10,5 +11,19 @@ def index():
 
 @app.route('/view360')
 def view360():
-    return render_template('view360.html')
+    panorama_url = request.args.get('url')
+    return render_template('view360.html', panorama_url=panorama_url)
+
+
+@app.route('/projeto')
+def projeto():
+  #  pasta = Path('/caminho/para/pasta')
+    return render_template('projeto.html')
+
+@app.route('/animado')
+def animacao():
+    baseurl='static/images/DJI_20241030151411_0046_D.JPG'
+    image='DJI_20241030151411_0046_D.JPG'
+    panorama_url = request.args.get('url')
+    return render_template('animacao.html', image=image, baseurl=baseurl)
 
